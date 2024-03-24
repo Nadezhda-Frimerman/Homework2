155e3fc1a519432d26bc00327aa9418074bf858b
Nadezhda-Frimerman <tomns@mail.ru>
2024-03-24 08:45:04 +0500

    Add "exercise #"

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,34 @@
 public class Main {
        public static void main(String[] args) {
+        System.out.println("exercise 1");
         var dog=8.0;
         var cat=3.6;
         var paper=763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println("exercise 2");
         dog=dog+4;
         cat=cat+4;
         paper=paper+4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println("exercise 3");
         dog=dog-3.5;
         cat=cat-1.6;
         paper=paper-7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println("exercise 4");
         var friend=19;
         System.out.println(friend);
         friend=friend+2;
         System.out.println(friend);
         friend=friend/7;
         System.out.println(friend);
+        System.out.println("exercise 5");
         var frog=3.5;
         System.out.println(frog);
         frog=frog*10;
@@ -35,8 +40,15 @@ public class Main {
         System.out.println("exercise 6");
         var weight1=78.2;
         var weight2=82.7;
-        System.out.println(weight1+weight2);
-        System.out.println(weight2-weight1);
+        var totalWeight=weight1+weight2;
+        var weightDifference=weight2-weight1;
+        System.out.println("total weight "+totalWeight+" kg.");
+        System.out.println("weight difference "+weightDifference+" kg.");
+        System.out.println("exercise 7");
+        var weight3=78.2;
+        var weight4=82.7;
+        var weightDifference1=weight4%weight3;
+        System.out.println("weight difference "+weightDifference1+" kg.");
 
 
 
